7296889172b58934b0878e6c43ee81acfef8602c
Zim <zimuzostanley@gmail.com>
2013-10-27 23:35:53 +0100

    working on the navbar quirk

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 $(function() {
 	$('body').scrollspy({ target: '#navbar-jjdf' });
+	$('[data-spy="scroll"]').each(function () {
+  		var $spy = $(this).scrollspy('refresh')
+	});
 	$('#read-more-about').click(function() {
 		$('.extra-about').css('display', 'block');
 		$(this).css('display', 'none');
