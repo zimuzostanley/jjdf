d858dde5b6af5606efd48061ecb8e8be434226c4
Zim <zimuzostanley@gmail.com>
2013-10-29 10:55:50 +0100

    New and redesigned layout with some colors and icons

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,4 @@
 $(function() {
-	$('body').scrollspy({ target: '#navbar-jjdf' });
-	$('[data-spy="scroll"]').each(function () {
-  		var $spy = $(this).scrollspy('refresh')
-	});
 	$('#read-more-about').click(function() {
 		$('.extra-about').css('display', 'block');
 		$(this).css('display', 'none');
