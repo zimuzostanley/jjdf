63615c483728721e2df975f46438c12902e8ee69
Zim <zimuzostanley@gmail.com>
2013-11-21 19:59:03 +0100

    New uncolourful page, added actual pictures, centralized heading, remaining founders picture

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,47 @@
 $(function() {
-	$('#read-more-about').click(function() {
-		$('.extra-about').css('display', 'block');
-		$(this).css('display', 'none');
+	$('.phone-desktop').click(function() {
+		$('.direction-wrapper').css('display', 'none');
+		$('.phone-wrapper').toggle();
+	});
+
+	$('.direction-desktop').click(function() {
+		$('.phone-wrapper').css('display', 'none');
+		$('.direction-wrapper').toggle();
+	});
+
+	$('#read-more-started').click(function() {
+		$('#read-more-started').css('display', 'none');
+		$('.extra-started').css('display', 'block');
+	});
+
+	$('#read-more-clement').click(function() {
+		$('#read-more-clement').css('display', 'none');
+		$('.extra-clement').css('display', 'block');
+	});
+
+	$('#read-more-ifeoma').click(function() {
+		$('#read-more-ifeoma').css('display', 'none');
+		$('.extra-ifeoma').css('display', 'block');
+	});
+
+	$('.accordion-heading').click(function() {
+		$('.extra-started').css('display', 'none');
+		$('.extra-clement').css('display', 'none');
+		$('.extra-ifeoma').css('display', 'none');
+
+		$('#read-more-started').css('display', 'inline');
+		$('#read-more-clement').css('display', 'inline');
+		$('#read-more-ifeoma').css('display', 'inline');
+	});
+
+	$('.phone-mobile').click(function() {
+		$('.direction-wrapper').css('display', 'none');
+		$('.phone-wrapper').toggle();
+	});
+
+	$('.direction-mobile').click(function() {
+		$('.phone-wrapper').css('display', 'none');
+		$('.direction-wrapper').toggle();
 	});
 });
 
