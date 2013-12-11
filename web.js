a3123fec5de039ec1c42966cc5243f6bc86f7231
Zim <zimuzostanley@gmail.com>
2013-12-11 19:39:50 +0100

    New Look with separate navigation pages

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
-var routes = require('./routes');
 
 var app = express();
 
@@ -15,7 +14,39 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', routes.index);
+app.get('/clement', function(req, res) {
+	res.render('clement', {});
+});
+
+app.get('/chinelo', function(req, res) {
+	res.render('chinelo', {});
+});
+
+app.get('/how-we-started', function(req, res) {
+	res.render('how-we-started', {});
+});
+
+app.get('/team', function(req, res) {
+	res.render('team', {});
+});
+
+app.get('/speech', function(req, res) {
+	res.render('speech', {});
+});
+
+app.get('/contact', function(req, res) {
+	res.render('contact', {});
+});
+
+app.get('/donate', function(req, res) {
+	res.render('donate', {});
+});
+
+app.get('/', function(req, res) {
+	res.render('index', {});
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
